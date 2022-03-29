d0e5de06acdac4dbb855e50b108f55ba550ef6ae
Anish S M <anishpapu@gmail.com>
2022-03-30 00:12:07 +0530

    Common Project

---
+++ Common/DeliVeggie.Common/Models/ProductDto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DeliVeggie.Common.Models
+{
+    public class ProductDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime EntryDate { get; set; }
+        public Double Price { get; set; }
+    }
+} 
