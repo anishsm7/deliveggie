559feb6f4d8f644db8a41cec85c4c1952138e42b
Anish S M <anishpapu@gmail.com>
2022-03-31 11:11:38 +0530

    UI corrections

--- Services/DeliVeggie.Services.Products.Tests/ProductTest.cs
+++ Services/DeliVeggie.Services.Products.Tests/ProductTest.cs
@@ -2,21 +2,22 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DeliVeggie.Services.Products.Manager;
 using Microsoft.Extensions.Configuration;
 using DeliVeggie.Services.Products.Interfaces;
+using DeliVeggie.Services.Products.Repository;
 
 namespace DeliVeggie.Services.Products.Test
 {
     [TestClass]
     public class ProductTest
     {
-
         public IConfiguration Configuration;
         private ProductManager productManager;
 
-        public ProductTest(IProductRepository productRepository)
+        public ProductTest()
         {
             this.Configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
             .Build();
+            IProductRepository productRepository = new ProductRepository(Configuration);
             this.productManager = new ProductManager(Configuration, productRepository);
         }
 
@@ -24,8 +25,15 @@ namespace DeliVeggie.Services.Products.Test
         public void GetProducts_ValidResponse()
         {
             var products = this.productManager.GetProducts();
-            Assert.IsNotNull(products, "Products should not be null");
-            Assert.IsTrue(products.Count > 0, "At least one product should be in database");
+            Assert.IsNotNull(products, "Products not null");
+            Assert.IsTrue(products.Count > 0, "Product count should not be0");
+        }
+
+         [TestMethod]
+        public void GetProduct_ValidResponse()
+        {
+            var product = this.productManager.GetProduct("6241ec2788c657a26d7ccdee");
+            Assert.IsNotNull(product, "Product details not null");            
         }
 
     }
